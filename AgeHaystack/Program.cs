c2d1fddc49d6c031c07597f903142ad9828a2b99
DavidTChacon <DavidTChacon@spamhereplease.com>
2017-03-24 16:59:39 -0400

    init commit

---
+++ AgeHaystack/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace AgeHaystack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var apiKey = "**API KEY**";
+            var apiEndpoint = String.Format("https://api.haystack.ai/api/image/analyze?output=json&apikey={0}&model=age", apiKey);
+
+            using (var client = new WebClient())
+            {
+                var image = File.ReadAllBytes(args[0]);
+                var responseData = client.UploadData(apiEndpoint, "POST", image);
+                var responseText = Encoding.UTF8.GetString(responseData);
+                dynamic response = JsonConvert.DeserializeObject(responseText);
+                var age = (String)response.people[0].age;
+
+                Console.WriteLine(age);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# AgeHaystack 
+Simple example project powered by Haystack.AI (https://www.haystack.ai/). Using Haystack.AI you can estimate the age of a person using a picture of their face.
